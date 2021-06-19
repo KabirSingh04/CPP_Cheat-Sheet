16d110e91f3668b2dcfa38ce030c754048258a0c
kabirsingh04 <kabirsingh0405@gmail.com>
2021-06-20 03:26:59 +0530

    New Program Added

---
+++ Introduction/Print-Number.cpp
@@ -0,0 +1,16 @@
+
+// Print a Number entered By the user
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+
+    cout<<"The entered Number is : "<<n;
+
+
+}
